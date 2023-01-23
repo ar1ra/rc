d1c04586ea1fc9b520f10942a749e3a7285dffa3
raman <orroman979@gmail.com>
2023-01-24 00:04:05 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { httpServer } from "./src/http_server/index.js";
 import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
+console.log(HTTP_PORT)
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
